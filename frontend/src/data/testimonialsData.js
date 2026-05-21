f2d3c78009fefbeb341e73e060d1eb259149613d
Marco Optimus <paul.ramz247@gmail.com>
2026-05-21 15:24:46 -0600

    Testimonials udpated (js, jsx and css) from Services component

--- frontend/src/data/testimonialsData.js
+++ frontend/src/data/testimonialsData.js
@@ -1,81 +1,61 @@
 const TESTIMONIALS = [
   {
     id: 1,
-    name: 'Juan Pérez',
+    name: 'Acid Rain (band)',
     role: 'Cliente',
-    title: 'Profesionales y comprometidos con la calidad. Recomendado.',
+    title: 'Buena comunicación, paciencia y atención.',
     content:
-      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.',
+      'Trabajar con REC-ON Studio, fue una experiencia muy profesional y cómoda. Desde el primer día hubo buena comunicación, paciencia y atención a cada detalle del proyecto. El equipo que tienen es el adecuado para el sonido que buscábamos, estamos satisfechos de los resultados finales.',
     portrait:
       'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
   },
   {
     id: 2,
-    name: 'María García',
+    name: 'Teona',
     role: 'Cliente',
-    title: 'Profesionales y comprometidos con la calidad. Recomendado.',
+    title: 'El estudio al que siempre regresaremos.',
     content:
-      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.',
+      'Desde que decidimos iniciar el proyecto, el sencillo y el EP, todo ha sido trabajado de alguna manera en REC-ON. En experiencias con otros estudios sin duda hay confianza, profesionalismo, esa visión de producción que el proyecto en cuestión necesita. Siempre podemos llegar al punto de producción que queremos gracias a que está abierta esa puerta de ida y vuelta sobre que poner o quitar.',
     portrait:
       'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
   },
   {
     id: 3,
-    name: 'María García',
+    name: 'Relisp (band)',
     role: 'Cliente',
-    title: 'Profesionales y comprometidos con la calidad. Recomendado.',
+    title: 'Atentos a las necesidades de los artistas.',
     content:
-      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.',
+      'Grabar y ensayar en REC- ON Studio es una experiencia grata y siempre divertida. Hay espacio para la creatividad, pero también para convivir y planear lo siguiente. El ambiente es muy ameno y siempre están atentos de las necesidades de los artistas y al resultado que se quiere llegar, recomendado sin lugar a duda. La ubicación es de fácil acceso y tranquila.',
     portrait:
       'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
   },
   {
     id: 4,
-    name: 'María García',
+    name: 'Carlo',
     role: 'Cliente',
-    title: 'Profesionales y comprometidos con la calidad. Recomendado.',
+    title: 'Profesionales en todos los ámbitos',
     content:
-      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.',
+      'Jorge es una persona muy paciente y atenta a todo lo que necesites o sugieras.En general todo el equipo que conforma y trabaja en REC-ON Studio son muy profesionales en todos los ámbitos.',
     portrait:
       'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
   },
   {
     id: 5,
-    name: 'María García',
+    name: 'Adrián',
     role: 'Cliente',
-    title: 'Profesionales y comprometidos con la calidad. Recomendado.',
+    title: 'Siempre con miras de buscar lo mejor del proyecto.',
     content:
-      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.',
+      'Trabajar con Jorge Trejo en el REC-ON Studio es excelente. Atento a todas las peticiones y siempre con miras de buscar lo mejor del proyecto. Trabaja rápido y bien. Además, el equipo del REC-ON es bueno, y está en buen estado. La salita fuera del estudio también es agradable y se puede pasar bien el rato.',
     portrait:
       'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
   },
   {
     id: 6,
-    name: 'María García',
+    name: 'Mario Sánchez',
     role: 'Cliente',
-    title: 'Profesionales y comprometidos con la calidad. Recomendado.',
+    title: 'Profesionalidad y calidad.',
     content:
-      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.',
-    portrait:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
-  },
-  {
-    id: 7,
-    name: 'María García',
-    role: 'Cliente',
-    title: 'Profesionales y comprometidos con la calidad. Recomendado.',
-    content:
-      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.',
-    portrait:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
-  },
-  {
-    id: 8,
-    name: 'María García',
-    role: 'Cliente',
-    title: 'Profesionales y comprometidos con la calidad. Recomendado.',
-    content:
-      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.',
+      'Trabajar en REC-ON estudio ha sido de las más gratificantes experiencias musicalmente hablando que he tenido, además de la profesionalidad y calidad que se obtiene al desarrollar un proyecto, Jorge es de las personas más cálidas y chingonas que se pueden conocer en el medio. 100000% recomendado grabar/mezclar/masterizar/ensayar en REC-ON ESTUDIO.',
     portrait:
       'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
   },
