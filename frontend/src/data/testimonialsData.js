c0731c75df3c881f166102e449d7252071346144
Marco Optimus <paul.ramz247@gmail.com>
2026-05-22 11:39:50 -0600

    testimonialsData.js updated and testimonial card re-styled

--- frontend/src/data/testimonialsData.js
+++ frontend/src/data/testimonialsData.js
@@ -7,17 +7,17 @@ const TESTIMONIALS = [
     content:
       'Trabajar con REC-ON Studio, fue una experiencia muy profesional y cómoda. Desde el primer día hubo buena comunicación, paciencia y atención a cada detalle del proyecto. El equipo que tienen es el adecuado para el sonido que buscábamos, estamos satisfechos de los resultados finales.',
     portrait:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
+      'https://ik.imagekit.io/novelisk/Rec-On/testimonials-portraits/acid_rain_mx.jpeg',
   },
   {
     id: 2,
-    name: 'Teona',
+    name: 'Teonanácatl (band)',
     role: 'Cliente',
     title: 'El estudio al que siempre regresaremos.',
     content:
       'Desde que decidimos iniciar el proyecto, el sencillo y el EP, todo ha sido trabajado de alguna manera en REC-ON. En experiencias con otros estudios sin duda hay confianza, profesionalismo, esa visión de producción que el proyecto en cuestión necesita. Siempre podemos llegar al punto de producción que queremos gracias a que está abierta esa puerta de ida y vuelta sobre que poner o quitar.',
     portrait:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
+      'https://ik.imagekit.io/novelisk/Rec-On/testimonials-portraits/teonanacatl.jpeg',
   },
   {
     id: 3,
@@ -27,27 +27,27 @@ const TESTIMONIALS = [
     content:
       'Grabar y ensayar en REC- ON Studio es una experiencia grata y siempre divertida. Hay espacio para la creatividad, pero también para convivir y planear lo siguiente. El ambiente es muy ameno y siempre están atentos de las necesidades de los artistas y al resultado que se quiere llegar, recomendado sin lugar a duda. La ubicación es de fácil acceso y tranquila.',
     portrait:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
+      'https://ik.imagekit.io/novelisk/Rec-On/testimonials-portraits/relisp.jpeg',
   },
   {
     id: 4,
-    name: 'Carlo',
+    name: 'Adrián',
     role: 'Cliente',
     title: 'Profesionales en todos los ámbitos',
     content:
-      'Jorge es una persona muy paciente y atenta a todo lo que necesites o sugieras.En general todo el equipo que conforma y trabaja en REC-ON Studio son muy profesionales en todos los ámbitos.',
+      'Jorge es una persona muy paciente y atenta a todo lo que necesites o sugieras. En general todo el equipo que conforma y trabaja en REC-ON Studio son muy profesionales en todos los ámbitos.',
     portrait:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
+      'https://ik.imagekit.io/novelisk/Rec-On/testimonials-portraits/adrian.jpeg',
   },
   {
     id: 5,
-    name: 'Adrián',
+    name: 'Carlo',
     role: 'Cliente',
     title: 'Siempre con miras de buscar lo mejor del proyecto.',
     content:
       'Trabajar con Jorge Trejo en el REC-ON Studio es excelente. Atento a todas las peticiones y siempre con miras de buscar lo mejor del proyecto. Trabaja rápido y bien. Además, el equipo del REC-ON es bueno, y está en buen estado. La salita fuera del estudio también es agradable y se puede pasar bien el rato.',
     portrait:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
+      'https://ik.imagekit.io/novelisk/Rec-On/testimonials-portraits/carlo.jpeg',
   },
   {
     id: 6,
@@ -57,8 +57,32 @@ const TESTIMONIALS = [
     content:
       'Trabajar en REC-ON estudio ha sido de las más gratificantes experiencias musicalmente hablando que he tenido, además de la profesionalidad y calidad que se obtiene al desarrollar un proyecto, Jorge es de las personas más cálidas y chingonas que se pueden conocer en el medio. 100000% recomendado grabar/mezclar/masterizar/ensayar en REC-ON ESTUDIO.',
     portrait:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg',
+      'https://ik.imagekit.io/novelisk/Rec-On/testimonials-portraits/mario_sanchez.JPG',
   },
+  {
+    id: 7,
+    name: 'Jorge Veliz',
+    role: 'Cliente',
+    title: 'Un estudio 10/10.',
+    content: 'Las instalaciones están buenísimas, cuentan con instrumentos, diferentes tipos de micrófonos y todo un equipo necesario y óptimo para crear buena música y hasta poder ensayar. La atención, la vibra y el lugar, muy recomendado.',
+    portrait: 'https://ik.imagekit.io/novelisk/Rec-On/testimonials-portraits/jorge_veliz.jpeg',
+  },
+  {
+    id: 8,
+    name: 'Frutas de Venus',
+    role: 'Cliente',
+    title: 'Profesionalis y gran trabajo.',
+    content: 'En 2025 grabamos nuestro EP debut en REC-ON Studio, un estudio que desde hace tiempo nos llamaba la atención debido a que varias bandas de rock de la CDMX han trabajado ahí. Tener la oportunidad de grabar nuestro primer material de estudio en ese lugar fue una gran experiencia. Quedamos completamente satisfechos con la calidad del trabajo, el trato recibido y la relación calidad-precio. Además de llevarnos un excelente resultado, también nos llevamos una amistad. Agradecemos especialmente a Jorge Trejo por su profesionalismo y el gran trabajo realizado.',
+    portrait: 'https://ik.imagekit.io/novelisk/Rec-On/testimonials-portraits/frutas_de_venus.jpeg',
+  },
+  // {
+  //   id: null,
+  //   name: '',
+  //   role: '',
+  //   title: '',
+  //   content: '',
+  //   portrait: '',
+  // }
 ];
 
 export default TESTIMONIALS;
